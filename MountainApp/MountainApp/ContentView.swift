040efdbfa97c01b6b68dd740e9d5bb4e59db1e96
takaoh717 <hrgc.tko0717@gmail.com>
2022-01-24 08:12:03 +0900

    add project

---
+++ MountainApp/MountainApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MountainApp
+//
+//  Created by horiguchi takao on 2022/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MountainApp/MountainApp/MountainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MountainAppApp.swift
+//  MountainApp
+//
+//  Created by horiguchi takao on 2022/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MountainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
